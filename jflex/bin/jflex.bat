38e8256ea5645b637310820e78844e1a31807a3e
a.malnev <a.malnev@gmail.com>
2019-09-18 09:59:25 +0300

    Windows build fix

--- jflex/bin/jflex.bat
+++ jflex/bin/jflex.bat
@@ -1,8 +1,7 @@
-@echo off
 REM Please adjust JFLEX_HOME to suit your needs
 REM (please do not add a trailing backslash)
 
-set JFLEX_HOME=C:\JFLEX
 set JFLEX_VERSION=1.7.0
+echo "%JFLEX_HOME%"
 
 java -Xmx128m -jar "%JFLEX_HOME%"\lib\jflex-full-%JFLEX_VERSION%.jar %*
